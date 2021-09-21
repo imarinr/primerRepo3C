eb5890b4942702e80dc72b21f6bf6d2b415a2b60
Ivan Marín <imarinr@outlook.com>
2021-09-21 12:48:31 +0000

    promedio, version del 21/09/21

---
+++ mi_promedio.py
@@ -0,0 +1,33 @@
+# Solicitar al usuario 5 calificaciones
+# obtener el promedio de estas
+
+# Algoritmo
+# 1. Inicio
+# 2. Definir n = 5
+n = 5
+# 3. Definir promedio, cali1, cali2, cali3, cali4, cali5
+# 4. Imprimir "Ingresa la calificacion 1"
+print("Ingresa la calificacion 1")
+# 5. cali1 = solicitar calificacion 1 al usuario por teclado
+cali1 = float(input())
+# 6. Imprimir "Ingresa la calificacion 2"
+print("Ingresa la calificacion 2")
+# 7. cali2 = solicitar calificacion 2 al usuario por teclado
+cali2 = float(input())
+# 8. Imprimir "Ingresa la calificacion 3"
+print("Ingresa la calificacion 3")
+# 9. cali3 = solicitar calificacion 3 al usuario por teclado
+cali3 = float(input())
+# 10. Imprimir "Ingresa la calificacion 4"
+print("Ingresa la calificacion 4")
+# 11. cali4 = solicitar calificacion 4 al usuario por teclado
+cali4 = float(input())
+# 12. Imprimir "Ingresa la calificacion 5"
+print("Ingresa la calificacion 5")
+# 13. cali5 = solicitar calificacion 5 al usuario por teclado
+cali5 = float(input())
+# 14. hacer promedio = (cali1 +cali2 + cali3 + cali4 + cali5) / n
+promedio = (cali1 + cali2 + cali3 + cali4 + cali5) / n
+# 15. Imprimir "Dadas tus calificaciones, tu promedio seria de ", promedio
+print("Dadas tus calificaciones, tu promedio seria de ", promedio)
+# 16. fin 
