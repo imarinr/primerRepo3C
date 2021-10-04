b004d6e7b3d1896387c891fb760909b290f3e924
Ivan Marín <imarinr@outlook.com>
2021-10-04 12:42:16 +0000

    Implemente el algoritmo basico para calcular un promedio

---
+++ implementacion_promedio.py
@@ -0,0 +1,24 @@
+# Esta es la implementación del algoritmo para obtener promedios
+# 1. Inicio
+# 2.definir variables
+# 3. dar valores
+
+# c se refiere a calificación
+c1 = 10
+c2 = 7.5
+c3 = 9
+c4 = 10
+c5 = 10
+
+# 4. Hacer promedio = c1 + c2 + c3 + c4 + c5 / 5
+
+# Jerarquia de operaciones
+# 0. Lo que sea que esté entre paréntesis
+# 1. potencias
+# 2. multiplicaciones y divisiones de izquierda a derecha
+# 3. sumas y restas
+promedio = (c1 + c2 + c3 + c4 + c5) / 5
+
+# 5. Imprmir promedio
+print("El promedio de tus calificaciones es:", promedio)
+# 6. fin
